3efdf75327696b177f90ba8166ed8304bb8c31f4
Narayan Khatri <narayankhatrig@gmail.com>
2025-11-06 15:36:25 +0900

    view todo

--- myApp/views.py
+++ myApp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+from myApp.models import TodoModel
+
 # Create your views here.
 
-
+#get the list of todos
+def view_todo(request):
+    todos = TodoModel.objects.all() #get all todos
+    return render(request, "view_todo.html", {"todos": todos},)
