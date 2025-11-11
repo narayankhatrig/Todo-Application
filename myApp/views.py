a687ba18807dfb4cf4e65e142e09e8235d3fbc01
Narayan Khatri <narayankhatrig@gmail.com>
2025-11-11 15:17:50 +0900

    css added

--- myApp/views.py
+++ myApp/views.py
@@ -8,13 +8,13 @@ from myApp.models import TodoModel
 #get the list of todos
 def view_todo(request):
     todos = TodoModel.objects.all() #get all todos
-    return render(request, "view_todo.html", {"todos": todos},)
+    return render(request, "css/view_todo.html", {"todos": todos},)
 
 #create the todos
 def add_todo(request):
     #print(request.method, request.POST)
     if request.method == "GET":
-        return render(request, "add_todo.html")
+        return render(request, "css/add_todo.html")
     else:
         TodoModel.objects.create(title=request.POST["title"], details=request.POST["details"])
         return HttpResponseRedirect("/") #takes to homepage
